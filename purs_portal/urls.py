760ac0b4406600bf3735eadc552dbaed39d2139d
TCHABODI Sadikou <sadsad2326@gmail.com>
2024-02-13 11:19:13 +0000

    integration de template ok

--- purs_portal/urls.py
+++ purs_portal/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import website.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', website.views.home, name='home'),
 ]
